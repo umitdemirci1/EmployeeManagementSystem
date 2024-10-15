d73820ffbeeae80c700c276abd58edc794d26561
umitd111 <umit.demirci1996@gmail.com>
2024-10-15 03:36:29 +0300

    base packages are installed. Identity is used. Db connection and db init is done. Core and DAL layers are created.

--- ProjeYonetimSistemi/Program.cs
+++ ProjeYonetimSistemi/Program.cs
@@ -1,4 +1,14 @@
+using DAL;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProjeYonetimSistemiContext")));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddRoleManager<RoleManager<IdentityRole>>();
 
 builder.Services.AddControllers();
 
