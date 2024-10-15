a3b3ef9755e5d25b28967e6feb41c50adc0cdb2f
umitd111 <umit.demirci1996@gmail.com>
2024-10-15 23:17:34 +0300

    HasQueryFilter for soft deleted datas is improved. IdentityRole table got the IsRemoved check.

---
+++ Core/IdentityModels/ApplicationRole.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.IdentityModels
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public bool IsDeleted { get; set; }
+    }
+}
